fd3d6099d95b32a4bd59ff6f40003263ff80b035
ashenoi <ashshenoi8@gmail.com>
2015-04-08 12:25:00 -0400

    Submitting changes in stage updated to infer based on bet

--- StageUpdater_Default.m
+++ StageUpdater_Default.m
@@ -19,20 +19,41 @@ end
 
 function oppo_dis = PredictHoleCards(info)
     oppo_dis = [];
+    old_oppo_dis = info.su_info;
     %% ----- FILL IN THE MISSING CODE ----- %%
+    if info.stage == 0
+        oppo_dis = zeros(info.num_oppo,169);
+        return
+    end
     board_card = info.board_card;
     board_card = board_card(board_card ~= -1);    
     bnet_model_card = generate_model_from_board(board_card);
     oppo_model = info.oppo{1,1};
     num_oppo = length(oppo_model);
+    oppo_index =0;
     for i=1:1:num_oppo
-        [combined_bnet,Bet] = combine_bnet(bnet_model_card,oppo_model(i));
-        engine = jtree_inf_engine(combined_bnet);
-        evidence = cell(1,length(combined_bnet.node_sizes));
-        evidence{Bet} = 1;
-        [engine, loglik] = enter_evidence(engine, evidence);
-        m = marginal_nodes(engine,1);
-        oppo_dis = [oppo_dis;(m.T)'];
+        if i == info.cur_pos
+            oppo_index = oppo_index+2;
+        else
+            oppo_index = oppo_index+1;
+        end
+        if info.stage_bet(oppo_index) ~= 0
+            
+            [combined_bnet,Bet] = combine_bnet(bnet_model_card,oppo_model(i));
+            engine = jtree_inf_engine(combined_bnet);
+            evidence = cell(1,length(combined_bnet.node_sizes));        
+            evidence{Bet} = info.stage_bet(oppo_index);
+            [engine, loglik] = enter_evidence(engine, evidence);
+            m = marginal_nodes(engine,1);
+            dis = (m.T)';
+        else
+            if isempty(old_oppo_dis)
+                dis = zeros(1,169);
+            else
+                dis = old_oppo_dis(i,:);
+            end
+        end
+        oppo_dis = [oppo_dis; dis];
     end
     
 end
